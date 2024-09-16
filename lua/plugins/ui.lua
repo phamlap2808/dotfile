04e253816f2e6ef7f0108e5461593256eb9ae1b4
lappg <lappg1@runsystem.net>
2024-09-16 13:35:30 +0700

    Initial commit for lazyvim dot file

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,70 @@
+return {
+  -- messages, cmdline and the popupmenu
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 5000,
+    },
+  },
+
+  -- animations
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.scroll = {
+        enable = false,
+      }
+    end,
+  },
+
+  -- filename
+  {
+    "b0o/incline.nvim",
+    dependencies = { "craftzdog/solarized-osaka.nvim" },
+    event = "BufReadPre",
+    priority = 1200,
+    config = function()
+      local colors = require("solarized-osaka.colors").setup()
+      require("incline").setup({
+        highlight = {
+          groups = {
+            InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
+            InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
+          },
+        },
+        window = { margin = { vertical = 0, horizontal = 1 } },
+        hide = {
+          cursorline = true,
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if vim.bo[props.buf].modified then
+            filename = "[+] " .. filename
+          end
+
+          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+          return { { icon, guifg = color }, { " " }, { filename } }
+        end,
+      })
+    end,
+  },
+
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    opts = function(_, opts)
+      local logo = [[
+    ██╗      █████╗ ██████╗ ██████╗  ██████╗ 
+    ██║     ██╔══██╗██╔══██╗██╔══██╗██╔════╝ 
+    ██║     ███████║██████╔╝██████╔╝██║  ███╗
+    ██║     ██╔══██║██╔═══╝ ██╔═══╝ ██║   ██║
+    ███████╗██║  ██║██║     ██║     ╚██████╔╝
+    ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝      ╚═════╝
+      ]]
+
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+      opts.config.header = vim.split(logo, "\n")
+    end,
+  },
+}
